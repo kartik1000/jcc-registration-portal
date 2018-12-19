d477fdb4b58bb0270b595464800208dd75004217
kartik1000 <43112279+kartik1000@users.noreply.github.com>
2018-12-19 16:39:26 +0530

    Validated PhoneNumber in models.py

--- core/models.py
+++ core/models.py
@@ -1,10 +1,11 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
+from phonenumber_field.modelfields import PhoneNumberField
 import uuid
 
 
-class Team(models.Model):
+class Team(models.Model, Importable):
     team_id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False)
     team_name = models.CharField(max_length=255, blank=True, null=True)
@@ -12,13 +13,13 @@ class Team(models.Model):
         max_length=255, default="No Player Assigned")
     player_one_email = models.EmailField(
         max_length=255, default="No Email Provided")
-    player_one_contact = models.BigIntegerField(blank=True, null=True)
+    player_one_contact = PhoneNumberField(null=False, blank=False, unique=True)
     player_one_hall = models.CharField(max_length=255, blank=True, null=True)
     player_two_name = models.CharField(
         max_length=255, default="No Player Assigned")
     player_two_email = models.EmailField(
         max_length=255, default="No Email Provided")
-    player_two_contact = models.BigIntegerField(blank=True, null=True)
+    player_two_contact = PhoneNumberField(null=False, blank=False, unique=True)
     player_two_hall = models.CharField(max_length=255, blank=True, null=True)
     timestamp = models.DateTimeField(default=timezone.now)
     unique_team_id = models.CharField(max_length=255, blank=True, null=True)
